8e1064d6f9b5f8a95f6c4da023ff2b3765676988
Vinnicius Castro <vinnicius.castro97@gmail.com>
2025-10-25 19:02:33 -0600

    query is not working

--- src/controller/routes/list.js
+++ src/controller/routes/list.js
@@ -1,14 +1,23 @@
-import { getAllRoutes, getListOfRegions, getListOfSeasons } from '../../models/model.js';
+import { getAllRoutes, getListOfRegions, getListOfSeasons, getRoutesByRegionAndSeason } from '../../models/model.js';
 
 export default async (req, res) => {
     const regions = await getListOfRegions();
-    const routes = await getAllRoutes();
+    // const routes = await getAllRoutes();
     const seasons = await getListOfSeasons();
-
+    
+    const region = req.query.regions;
+    const season = req.query.seasons;
+    const routes = await getRoutesByRegionAndSeason(region, season);
+    
     res.render('routes/list', { 
         title: 'Scenic Train Routes',
         regions,
         routes,
         seasons
     });
+
+
+
+    
 };
+
